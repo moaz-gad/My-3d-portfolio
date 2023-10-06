5a1cf4c056177a6693cc5d6b0171d0d493ab0317
moaz <moaz.a.gad@gmail.com>
2023-10-07 01:10:25 +0300

    Final=3

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
 
   useEffect(() => {
     // Add a listener for changes to the screen size
-    const mediaQuery = window.matchMedia("(max-width: 500px)")
+    const mediaQuery = window.matchMedia("(max-width: 600px)")
 
     // Set the initial value of the `isMobile` state variable
     setIsMobile(mediaQuery.matches)
