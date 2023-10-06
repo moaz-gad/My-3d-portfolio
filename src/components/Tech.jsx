7a2bce62e13b3c2c2d241048cf63eea105a2f9fa
moaz <moaz.a.gad@gmail.com>
2023-10-06 20:48:31 +0300

    Final=1

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -2,17 +2,15 @@ import { BallCanvas } from "./canvas"
 import { SectionWrapper } from "../hoc"
 import { technologies } from "../constants"
 
-const Tech = ({ isMobile }) => {
+const Tech = ({}) => {
   return (
-    !isMobile && (
-      <div className="flex flex-row flex-wrap justify-center gap-10">
-        {technologies.map((technology) => (
-          <div className="w-28 h-28" key={technology.name}>
-            <BallCanvas icon={technology.icon} />
-          </div>
-        ))}
-      </div>
-    )
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((technology) => (
+        <div className="w-28 h-28" key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
+    </div>
   )
 }
 
